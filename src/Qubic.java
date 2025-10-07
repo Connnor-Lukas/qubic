510feec42b80062d25cbd641f35f7a27508f8c01
eConnah <63052937+eConnah@users.noreply.github.com>
2025-10-07 14:03:25 +0200

    Created the main Qubic class with a main method.

---
+++ src/Qubic.java
@@ -0,0 +1,13 @@
+/**
+ * The main project class.
+ *
+ * @author Connor Alecks
+ * @ID 2254441
+ * @author Lukáš Hlaváč
+ * @ID 2278081
+ */
+public class Qubic {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
