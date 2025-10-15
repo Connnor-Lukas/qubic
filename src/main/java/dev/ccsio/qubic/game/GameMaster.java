d3affec4908218e414875b3a0b9565ddba60dbf6
eConnah <63052937+eConnah@users.noreply.github.com>
2025-10-15 10:15:49 +0200

    Convert GameMaster to singleton

--- src/main/java/dev/ccsio/qubic/game/GameMaster.java
+++ src/main/java/dev/ccsio/qubic/game/GameMaster.java
@@ -4,29 +4,43 @@ import dev.ccsio.qubic.types.Coordinates;
 
 
 public class GameMaster {
+    private static GameMaster INSTANCE;
+    private boolean initialised = false;
+
     String gameMode;
     GameBoard gameBoard;
     int mark;
     Boolean playersTurn;
     OpponentAlgorithm opponentAlgorithm;
     
+    private GameMaster() {}
 
-    public GameMaster(String gameMode, int difficulty) {
-        if (gameMode.toLowerCase() == "sp"  // sp: singe-player, tp: two-player
-            || gameMode.toLowerCase() == "tp") {  
-            this.gameMode = gameMode;  
-        } else {
-            throw new IllegalArgumentException("gameMode has to either be 'sp' or 'tp'");
+    public static GameMaster getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new GameMaster();
         }
+        return INSTANCE;
+    }
 
-        if (gameMode == "sp") {
-            this.opponentAlgorithm = new OpponentAlgorithm(difficulty);
+    public void init(String gameMode, int difficulty) {
+        if (!initialised) {
+            if (gameMode.toLowerCase() == "sp"  // sp: singe-player, tp: two-player
+                    || gameMode.toLowerCase() == "tp") {
+                this.gameMode = gameMode;
+            } else {
+                throw new IllegalArgumentException("gameMode has to either be 'sp' or 'tp'");
+            }
+
+            if (gameMode == "sp") {
+                this.opponentAlgorithm = new OpponentAlgorithm(difficulty);
+            }
+
+            this.gameBoard = new GameBoard();
+            this.mark = -1;
+            this.playersTurn = true;
+
+            initialised = true;
         }
-
-        this.gameBoard = new GameBoard();
-        this.mark = -1;
-        this.playersTurn = true;
-
     }
 
     public void handleInput(Coordinates input, Boolean isOA) {
