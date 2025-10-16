da1b22bd964a705c57afc7d867a8609fd61a1d33
eConnah <63052937+eConnah@users.noreply.github.com>
2025-10-16 10:10:38 +0200

    Update Colours.java

--- src/main/java/dev/ccsio/qubic/ui/common/Colours.java
+++ src/main/java/dev/ccsio/qubic/ui/common/Colours.java
@@ -1,8 +1,13 @@
 package dev.ccsio.qubic.ui.common;
 
 public class Colours {
-    public static final String CUSTOM_BLUE = "#4287f5";
-    public static final String CUSTOM_RED = "#FF4B4B";
-    public static final String CUSTOM_BLACK = "#2A2A2A";
-    public static final String CUSTOM_WHITE = "#FFFFFF";
+    // Menu
+    public static final String CUSTOM_MENU_BLUE = "#4287f5";
+    public static final String CUSTOM_MENU_RED = "#FF4B4B";
+    public static final String CUSTOM_MENU_BLACK = "#2A2A2A";
+    public static final String CUSTOM_MENU_WHITE = "#FFFFFF";
+
+    // 3D
+    public static final String CUSTOM_3D_RED = "#FF6666";
+    public static final String CUSTOM_3D_BLUE = "#6666FF";
 }
