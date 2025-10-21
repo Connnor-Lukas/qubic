cbaeeed19ec6c6d5b62fd98237c132e62d4cde8a
eConnah <63052937+eConnah@users.noreply.github.com>
2025-10-21 18:39:44 +0200

    Implement difficulty selector

--- src/main/java/dev/ccsio/qubic/ui/QubicWindow.java
+++ src/main/java/dev/ccsio/qubic/ui/QubicWindow.java
@@ -1,10 +1,13 @@
 package dev.ccsio.qubic.ui;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QubicWindow extends JFrame {
     private static QubicWindow INSTANCE;
     private JPanel currentView;
+    private List<JPanel> pastPanels = new ArrayList<>();
 
     private QubicWindow() {}
 
@@ -24,6 +27,7 @@ public class QubicWindow extends JFrame {
         if (currentView != null) {
             remove(currentView);
         }
+        pastPanels.add(newView);
         currentView = newView;
         add(currentView);
         revalidate();
@@ -35,5 +39,18 @@ public class QubicWindow extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(1200, 800);
         setLocationRelativeTo(null);
+    }
+
+    public JPanel getPastPanel(int panelsAgo) {
+        if (panelsAgo >= pastPanels.size()) {
+            throw new IndexOutOfBoundsException("There have only been " + pastPanels.size() + " panels in this window, not " + (panelsAgo + 1) + ".");
+        }
+        if (pastPanels.isEmpty()) {
+            throw new IndexOutOfBoundsException("There have been no panels in this window.");
+        }
+        if (panelsAgo < 0) {
+           throw new IndexOutOfBoundsException("Cannot get future panels.");
+        }
+        return pastPanels.get(pastPanels.size() - 1 - panelsAgo);
     }
 }
